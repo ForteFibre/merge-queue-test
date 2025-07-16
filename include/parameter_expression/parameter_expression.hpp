98925dede01f8e673e8e79dc0db55b7c609c5286
f0reachARR <f0reach@f0reach.me>
2025-07-16 13:08:07 +0900

    Apply formatter

--- include/parameter_expression/parameter_expression.hpp
+++ include/parameter_expression/parameter_expression.hpp
@@ -20,24 +20,26 @@
 #include <rclcpp/node_interfaces/node_parameters_interface.hpp>
 #include <rclcpp/parameter_event_handler.hpp>
 
-namespace parameter_expression {
+namespace parameter_expression
+{
 // ROS 2 dynamic typed parameter with mathematical expression
-class ParameterExpression {
+class ParameterExpression
+{
 public:
   ParameterExpression(
-      rclcpp::node_interfaces::NodeParametersInterface::SharedPtr
-          node_parameters_interface,
-      const std::string &name, const double default_value,
-      rcl_interfaces::msg::ParameterDescriptor descriptor);
+    rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_interface,
+    const std::string & name, const double default_value,
+    rcl_interfaces::msg::ParameterDescriptor descriptor);
 
   template <typename Node>
-  ParameterExpression(Node &node, const std::string &name,
-                      const double default_value = 0.0,
-                      rcl_interfaces::msg::ParameterDescriptor descriptor =
-                          rcl_interfaces::msg::ParameterDescriptor())
-      : ParameterExpression(
-            rclcpp::node_interfaces::get_node_parameters_interface(node), name,
-            default_value, descriptor) {}
+  ParameterExpression(
+    Node & node, const std::string & name, const double default_value = 0.0,
+    rcl_interfaces::msg::ParameterDescriptor descriptor =
+      rcl_interfaces::msg::ParameterDescriptor())
+  : ParameterExpression(
+      rclcpp::node_interfaces::get_node_parameters_interface(node), name, default_value, descriptor)
+  {
+  }
 
   virtual ~ParameterExpression();
 
@@ -46,20 +48,17 @@ public:
   RCLCPP_SMART_PTR_DEFINITIONS(ParameterExpression)
 
 private:
-  rcl_interfaces::msg::SetParametersResult
-  on_parameter(const std::vector<rclcpp::Parameter> &);
+  rcl_interfaces::msg::SetParametersResult on_parameter(const std::vector<rclcpp::Parameter> &);
   void eval(const rclcpp::Parameter parameter_value);
 
   void eval_first();
 
-  rclcpp::node_interfaces::NodeParametersInterface::SharedPtr
-      node_parameters_interface_;
-  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr
-      callback_handle_;
+  rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_interface_;
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr callback_handle_;
   rclcpp::Parameter parameter_;
   const std::string name_;
   mu::Parser parser_;
   double value_;
   const double default_value_;
 };
-} // namespace parameter_expression
+}  // namespace parameter_expression
